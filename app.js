8407e39dfef571dfbbc7ca1b2faa28599703eee9
ERSAGYN <ersagyn0@gmail.com>
2024-02-08 11:48:09 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const multer = require("multer");
+const bookController = require("./controllers/bookController");
+require("dotenv").config();
+
+const app = express();
+const port = 3001;
+app.use(express.json());
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app
+  .route("/api/books")
+  .get(bookController.getAllBooks)
+  .post(bookController.setNewBook);
+app.route("/api/books/byName/:name").get(bookController.getBookByName);
+app.route("/api/books/byPrice/:price").get(bookController.getBookByPrice);
+app
+  .route("/upload-excel")
+  .post(upload.single("file"), bookController.uploadBook);
+app
+  .route("/api/books/:id")
+  .put(bookController.updateBook)
+  .delete(bookController.deleteBook);
+app.route("/api/authors").get(bookController.getAuthors);
+app.route("/api/genres").get(bookController.getGenres);
+app.route("/api/author/:name/books").get(bookController.getBooksByAuthor);
+app.route("/api/genre/:genre/books").get(bookController.getBooksByGenre);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ controllers/bookController.js
@@ -0,0 +1,426 @@
+const ExcelJS = require("exceljs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const nodemailer = require("nodemailer");
+
+const accessToken =
+  "vk1.a.PyNnoFrrVj2u-E-L75nZd0kbOm1aBX9YjHV5C5HP7PtJHI0BGtG9lFSh5IwSGUxJ2e2v8rf9UUObFihfO-Jf5wy0uO7p5mjxrXDblnaeyoChmOwvKMgPNNaj-ly3OTUMPmKKKyr8UPb1ZP5SOqqjWgI16zA0LDYwiZmHaLrzM0cuBRIsX3llnykbG8-afB2szalFiF4_8Ib7KslQEB_aUg\n";
+const userId = "457299835";
+const randomId = Math.floor(Math.random() * 1000000);
+
+const transporter = nodemailer.createTransport({
+  service: "outlook",
+  auth: {
+    user: "220642@astanait.edu.kz",
+    pass: "kLrk1ZRhMmZhm",
+  },
+});
+
+async function getAllBooks(req, res) {
+  try {
+    const allBooks = await prisma.books.findMany();
+    res.status(200).json({
+      status: "success",
+      results: allBooks.length,
+      data: {
+        books: allBooks,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function setNewBook(req, res) {
+  try {
+    const { Name, Author, Genres, PagesCount, Price, PublishYear } = req.body;
+
+    const newBook = await prisma.books.create({
+      data: {
+        Name,
+        Author,
+        Genres,
+        PagesCount,
+        Price,
+        PublishYear,
+      },
+    });
+
+    const mailOptions = {
+      from: "220642@astanait.edu.kz",
+      to: "ersagyn0@gmail.com",
+      subject: "New Book Added",
+      text: "A new book has been added to the library.",
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Email notification error:", error);
+      } else {
+        console.log("Email notification sent:", info.response);
+      }
+    });
+
+    const message = "A new book has been added to the library.";
+    const vkApiUrl = `https://api.vk.com/method/messages.send?user_id=${userId}&message=${encodeURIComponent(message)}&access_token=${accessToken}&v=5.131&random_id=${randomId}`;
+
+    const vkApiResponse = await fetch(vkApiUrl, { method: "POST" });
+    const vkApiResult = await vkApiResponse.json();
+
+    if (vkApiResult.error) {
+      console.error("VK notification error:", vkApiResult.error);
+    } else {
+      console.log("VK notification sent:", vkApiResult.response);
+    }
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        book: newBook,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function uploadBook(req, res) {
+  try {
+    const { file } = req;
+
+    if (!file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.load(file.buffer);
+
+    const worksheet = workbook.worksheets[0];
+
+    console.log("Worksheet:", worksheet);
+    console.log("Columns:", worksheet.columns);
+    console.log("RowCount:", worksheet.rowCount);
+
+    const rows = worksheet.getSheetValues();
+
+    console.log("Rows:", rows);
+
+    const headers = rows[1];
+
+    for (let i = 2; i < rows.length; i++) {
+      const row = rows[i];
+      const data = {};
+
+      for (let j = 2; j < headers.length; j++) {
+        if (headers[j] === "Name") data[headers[j]] = String(row[j]);
+        else data[headers[j]] = row[j];
+      }
+
+      await prisma.books.create({ data });
+    }
+
+    res.json({
+      message: "Excel file uploaded and data inserted successfully",
+    });
+  } catch (error) {
+    console.error("Error uploading Excel file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+async function updateBook(req, res) {
+  try {
+    const bookId = parseInt(req.params.id, 10);
+    const { Name, Author, Genres, PagesCount, Price, PublishYear } = req.body;
+
+    const updatedBook = await prisma.books.update({
+      where: { id: bookId },
+      data: {
+        Name,
+        Author,
+        Genres,
+        PagesCount,
+        Price,
+        PublishYear,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book: updatedBook,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function deleteBook(req, res) {
+  try {
+    const bookId = parseInt(req.params.id, 10);
+
+    const existingBook = await prisma.books.findUnique({
+      where: { id: bookId },
+    });
+
+    if (!existingBook) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Book not found",
+      });
+    }
+
+    await prisma.books.delete({
+      where: { id: bookId },
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Book deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getBookByName(req, res) {
+  try {
+    const bookName = req.params.name;
+
+    // Query the database for the book with the specified name
+    const book = await prisma.books.findFirst({
+      where: { Name: bookName },
+    });
+
+    if (!book) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getBookByPrice(req, res) {
+  try {
+    const bookPrice = parseFloat(req.params.price);
+
+    // Query the database for the book with the specified name
+    const book = await prisma.books.findFirst({
+      where: { Price: bookPrice },
+    });
+
+    if (!book) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Book not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getAuthors(req, res) {
+  try {
+    const authors = await prisma.books.findMany({
+      distinct: ["Author"],
+      select: {
+        Author: true,
+      },
+    });
+
+    if (!authors || authors.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Authors not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        authors,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getGenres(req, res) {
+  try {
+    const books = await prisma.books.findMany();
+
+    if (!books || books.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Books not found",
+      });
+    }
+
+    const allGenres = books
+      .flatMap((book) => book.Genres.replace(/\[|\]|'/g, "").split(", "))
+      .filter(Boolean)
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        genres: allGenres,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getBooksByAuthor(req, res) {
+  try {
+    const authorName = req.params.name;
+
+    const booksByAuthor = await prisma.books.findMany({
+      where: {
+        Author: authorName,
+      },
+    });
+
+    if (!booksByAuthor || booksByAuthor.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: `Books by the author ${authorName} not found`,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        books: booksByAuthor,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function getBooksByGenre(req, res) {
+  try {
+    // Extract the genre from the parameters
+    const genre = req.params.genre;
+
+    // Query the database for books with the specified genre
+    const booksByGenre = await prisma.books.findMany({
+      where: {
+        Genres: {
+          contains: genre,
+        },
+      },
+    });
+
+    if (!booksByGenre || booksByGenre.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: `Books with the specified genre not found`,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        books: booksByGenre,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+module.exports = {
+  getAllBooks,
+  uploadBook,
+  setNewBook,
+  updateBook,
+  deleteBook,
+  getBookByName,
+  getBookByPrice,
+  getAuthors,
+  getGenres,
+  getBooksByAuthor,
+  getBooksByGenre,
+};
